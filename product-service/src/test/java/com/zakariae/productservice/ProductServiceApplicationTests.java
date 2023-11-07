09bacd7486f2ad77d4cd86046a60eb252d340518
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-11-07 23:56:23 +0100

    write test for update , not stable version

--- product-service/src/test/java/com/zakariae/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/zakariae/productservice/ProductServiceApplicationTests.java
@@ -17,6 +17,8 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zakariae.productservice.dto.ProductRequest;
+import com.zakariae.productservice.dto.ProductResponse;
+import com.zakariae.productservice.model.Product;
 import com.zakariae.productservice.repository.ProductRepository;
 
 @SpringBootTest
@@ -51,8 +53,46 @@ class ProductServiceApplicationTests {
 		Assertions.assertEquals(1, productRepository.findAll().size()); //checks if the product is added to the database
 	}
 
+	    void UpdateProduct() throws Exception {
+        ProductRequest productRequest = getProductRequest();
+		Product productResponse = getProductResponse();
+	
+        Product savedProduct =(Product)productRepository.save(productResponse) ; // Assuming you have a Product class
+        productRequest.setName("Updated Product");
+        String updatedProductString = objectMapper.writeValueAsString(productRequest);
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/product/{id}", savedProduct.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(updatedProductString))
+                .andExpect(status().isOk()); // checks if the response is "Product updated successfully"
+
+        Product updatedProduct = productRepository.findById(savedProduct.getId()).orElse(null);
+        Assertions.assertNotNull(updatedProduct);
+        Assertions.assertEquals("Updated Product", updatedProduct.getName());
+    }
+
+    @Test
+    void DeleteProduct() throws Exception {
+       Product productResponse = getProductResponse();
+        Product savedProduct = productRepository.save(productResponse);
+		System.out.println(savedProduct.getId());
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/product/{id}", savedProduct.getId()))
+                .andExpect(status().isOk()); // checks if the response is "Product deleted successfully"
+
+        Product deletedProduct = productRepository.findById(savedProduct.getId()).orElse(null);
+        Assertions.assertNull(deletedProduct);
+    }
 	private ProductRequest getProductRequest() {
 		return ProductRequest.builder()
+				.name("Product 1")
+				.description("Product 1 description")
+				.price(10.0)
+				.build();
+	}
+
+	private Product getProductResponse() {
+		return Product.builder()
+				.id("1")
 				.name("Product 1")
 				.description("Product 1 description")
 				.price(10.0)
